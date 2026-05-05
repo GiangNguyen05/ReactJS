96189eba266f6132cb0bb1197ddd23e4969eec9f
GiangNguyen05 <giangnguyen28052000@gmail.com>
2026-05-05 20:32:34 +0700

    update: main.js file

--- Day1/main.js
+++ Day1/main.js
@@ -6,3 +6,20 @@ console.log(name.myVar);
 name.myFunction();
 console.log(object);
 btn;
+
+// Spread Operator
+let soThich = ["Đọc sách", "Du lịch", "Nấu ăn"];
+const soThichMoi = "Xem phim";
+// soThich.push(soThichMoi);
+soThich = [...soThich, soThichMoi];
+console.log(soThich);
+
+let user = {
+  name: "John",
+  age: 30,
+};
+const userMoi = {
+  email: "gmail.com",
+};
+user = { ...user, ...userMoi };
+console.log(user);
